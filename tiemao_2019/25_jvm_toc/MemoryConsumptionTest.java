ca3b5c1b2da104ec35497d1c82fbb9b40f5bd7ea
renfufei <renfufei@qq.com>
2019-08-19 20:36:44 +0800

    部分资源地址

---
+++ tiemao_2019/25_jvm_toc/MemoryConsumptionTest.java
@@ -0,0 +1,21 @@
+package com.hb;
+
+/**
+ *
+ */
+public class MemoryConsumptionTest {
+
+
+    public static class BaseId {
+        protected long id;
+    }
+    public static class User extends BaseId{
+        protected int age;
+    }
+    public static class VipUser extends User{
+        protected boolean validState;
+    }
+    public static class SvipUser extends VipUser{
+        protected int vipLevel;
+    }
+}
---
+++ tiemao_2019/25_jvm_toc/javase_8_docs_index.md
@@ -0,0 +1,11 @@
+Java Platform Standard Edition 8 Documentation
+==
+
+![](desc_jdk_structure.jpg)
+
+
+参考链接: <https://docs.oracle.com/javase/8/docs/index.html>
+
+
+
+--
